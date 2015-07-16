22ed0b5032ed3780b5e1f51334d72f00b9aaf00d
kchow2 <kevinchow_23708@hotmail.com>
2015-07-15 19:28:56 -0700

    removed dead code

--- src/InvertedHeatMapColourProvider.cpp
+++
@@ -1,5 +0,0 @@
-#include "InvertedHeatMapColourProvider.h"
-
-HeatMapColourProvider::Colour InvertedHeatMapColourProvider::getHeatMapColour(double heatValue){
-	return HeatMapColourProvider::getHeatMapColour(1.0-heatValue);
-}
--- src/InvertedHeatMapColourProvider.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "HeatMapColourProvider.h"
-
-class InvertedHeatMapColourProvider: public HeatMapColourProvider
-{
-public:
-	virtual HeatMapColourProvider::Colour getHeatMapColour(double heatValue);
-};
-
